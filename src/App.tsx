f6034b8d76846f4c451ac91f74f6703fda4099f1
Mr. Horizon <horizon1628@gmail.com>
2026-07-23 16:31:48 +0530

    Add Brand Introduction section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navigation from "./components/layout/Navigation";
 import Hero from "./sections/hero/Hero";
+import BrandIntroduction from "./sections/home/BrandIntroduction";
 
 export default function App() {
   return (
@@ -8,7 +9,8 @@ export default function App() {
 
                   <main>
                           <Hero />
-                                </main>
+                            <BrandIntroduction />
+                            </main>
                                     </div>
                                       );
                                       }
